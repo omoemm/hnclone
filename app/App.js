f709fb6992934cdc98f1c654434fea7b4f71f849
omoemm <omoemm@pm.me>
2021-01-04 08:10:58 -0500

    Add the navbar

--- app/App.js
+++ app/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './index.css'
+import Nav from './components/Nav'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+        </div>
+      </Router>
     )
   }
 }
---
+++ app/components/Nav.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { NavLink, Redirect } from 'react-router-dom'
+
+const activeStyle = {
+  color: 'rgb(187, 46, 31)',
+}
+
+export default function Nav() {
+  return (
+    <nav className='row space-between'>
+      <ul className='row nav'>
+        <li>
+          <NavLink
+            to="/"
+            exact
+            activeStyle={activeStyle}
+            className='nav-link'
+          >
+            Top
+            </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/new"
+            exact
+            activeStyle={activeStyle}
+            className='nav-link'
+          >
+            New
+            </NavLink>
+        </li>
+      </ul>
+      <button
+        style={{ fontSize: 30 }}
+        className='btn-clear'>
+        🔦
+      </button>
+    </nav>
+  )
+}
