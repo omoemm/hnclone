f85f1e9725a24d70f7e280c3542644d378e00bdb
omoemm <omoemm@pm.me>
2021-01-04 07:52:53 -0500

    Add Hello World

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
