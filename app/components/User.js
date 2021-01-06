ab71123136856df955aa05d02f371945bd61f89f
omoemm <omoemm@pm.me>
2021-01-06 08:41:00 -0500

    Add the building blocks of User api

--- app/components/User.js
+++ app/components/User.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import queryString from 'query-string'
+import { getUserData } from '../utils/api'
 
 export default class User extends React.Component {
   state = {id:'loading'}
 
   componentDidMount() {
     const { id } = queryString.parse(this.props.location.search)
-    this.setState({ id })
+
+    getUserData(id).then(
+      result => {this.setState(result)}
+    )
   }
 
   render() {
-    const { id } = this.state
-    return <p>hello {id} </p>
+    return <p>hello {JSON.stringify(this.state)}</p>
   }
 }
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,7 @@
 const maxPosts = 25;
 
 function getPostIds(category) {
-  if (category !== 'top' && category !=='new') {
+  if (category !== 'top' && category !== 'new') {
     throw `invalid category: ${category}`
   }
 
@@ -14,6 +14,16 @@ function getPost(id) {
   const endpoint = window.encodeURI(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
   return fetch(endpoint)
     .then(response => response.json())
+}
+
+function getUser(id) {
+  const endpoint = window.encodeURI(`https://hacker-news.firebaseio.com/v0/user/${id}.json`)
+  return fetch(endpoint)
+    .then(response => response.json())
+}
+
+export function getUserData(id) {
+  return getUser(id)
 }
 
 export function getPosts(category) {
